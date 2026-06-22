a8fb8be53689b093be5de71ec6d12fe256ae4429
shreyadudhagave <shreyadudhagave@gmail.com>
2026-06-22 23:03:37 +0530

    Update CORS origin to production URL

--- Backend/index.js
+++ Backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://interviewiq-ai-sr27.onrender.com",
     credentials:true
 }));
 
